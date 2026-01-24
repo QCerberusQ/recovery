3d38db0762cdb8fe7fee511970d5496e6f50513e
Furkan Enes <113352008+QCerberusQ@users.noreply.github.com>
2026-01-24 22:19:53 +0300

    Update orangefox.sh

--- nothing-spacewar/orangefox.sh
+++ nothing-spacewar/orangefox.sh
@@ -10,7 +10,7 @@ git clone https://gitlab.com/OrangeFox/sync
 env --chdir=sync bash orangefox_sync.sh --branch 12.1 --path $(pwd)/build
 cd build
 git clone --depth=1 https://github.com/QCerberusQ/orangefox_device_nothing_Spacewar device/nothing/Spacewar
-git clone --depth=1 https://github.com/LineageOS/android_kernel_nothing_sm7325 kernel/nothing/sm7325
+#git clone --depth=1 https://github.com/LineageOS/android_kernel_nothing_sm7325 kernel/nothing/sm7325
 mkdir -p device/nothing/Spacewar/prebuilt/dtbs
 
 # extract dtb and vendor-ramdisk
